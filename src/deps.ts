5d18361411c9236b4d8719511834c605c94ae86c
Babiuch Family <babiuch@Babiuchs-MacBook-Pro.local>
2023-09-20 20:54:38 -0700

    Stood up initial service and project structure.

---
+++ src/deps.ts
@@ -0,0 +1,2 @@
+export { Router, Application } from "https://deno.land/x/oak/mod.ts";
+export { Logger}  from "https://deno.land/x/logger@v1.1.2/mod.ts";
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Application,
+     Logger,
+      Router } from "./deps.ts";
+
+const logger: Logger = new Logger();
+await logger.initFileLogger("../log");
+
+const app: Application = new Application();
+const router: Router = new Router();
+app.use(async (ctx, next) => {
+    await next();
+    logger.info(`${ctx.request.method} ${ctx.request.url}`);
+})
+
+router.get("/api/v1", (ctx) => {
+    ctx.response.body = "Hello!";
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({hostname, port, secure}) => {
+    logger.info(`Listening on ${hostname}:${port}`);
+});
+await app.listen({ port: 8000});
